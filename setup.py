2494569a182f5ea47671fd2597c89de04de95a88
0xidm <0xidm@protonmail.com>
2023-07-24 11:22:07 -0400

    regularize name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     version='0.1.0',
-    name='Nested Filestore',
+    name='nested-filestore',
     description="Nested Filestore",
     packages=[
         'nested_filestore',
